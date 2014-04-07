efa36623ad64b5096a34085afac9111f23fc0e58
Gustavo Braganca <ga.braganca@gmail.com>
2014-04-07 09:45:48 +0200

    Adds docstring to package.

--- instprofile/__init__.py
+++ instprofile/__init__.py
@@ -1,3 +1,22 @@
+"""
+Instrumental Profile
+====================
+
+When one observes the spectrum of a source of light, the x-axis of the
+spectrum is in pixels. To convert this to wavelengths, it is used a spectrum
+of a well know source of light, e.g., the light of a lamp of ThAr. The
+spectrum of the lamp has known emission lines, thus it is possible to convert
+from pixel to wavelength.
+
+From theory, the emission lines of the lamp spectrum should be infinitely
+narrow. Unfortunately, this does not happens because of the optics of the
+instrument, which broadens the spectrum. When studying spectral lines of
+unknown sources, this broadening effect has to be considered.
+
+For an example of how to use it, see this `link`__.
+
+__ http://nbviewer.ipython.org/urls/raw.githubusercontent.com/gabraganca/instprofile/master/example/Instrumental_Profile.ipynb
+"""
 import sys
 import numpy as np
 from scipy.optimize import leastsq
